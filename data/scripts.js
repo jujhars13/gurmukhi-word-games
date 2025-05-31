982ab01b292fe4c43db13d6f81796c941c3d686c
Jujhar Singh <jujhar@jujhar.com>
2025-05-31 10:31:05 +0100

    ✨ (editorconfig): add .editorconfig file to maintain consistent coding styles across the project ✨ (index.html): create index.html file for Gurmukhi Word Games with basic structure and Tailwind CSS integration ✨ (scripts.js): add scripts.js file to define Gurmukhi alphabet data and initialize document readiness for further functionality

---
+++ data/scripts.js
@@ -0,0 +1,67 @@
+const akhar={
+    "lines": [
+    {
+        "ੳ": "a",
+        "ਅ": "aa",
+        "ੲ": "i",
+        "ਸ": "s",
+        "ਹ": "h"
+    },
+    {
+        "ਕ": "k",
+        "ਖ": "kh",
+        "ਗ": "g",
+        "ਘ": "gh",
+        "ਙ": "ng"
+    },
+    {
+        "ਚ": "ch",
+        "ਛ": "chh",
+        "ਜ": "j",
+        "ਝ": "jh",
+        "ਞ": "ny"
+    },
+    {
+        "ਟ": "tt",
+        "ਠ": "tth",
+        "ਡ": "dd",
+        "ਢ": "ddh",
+        "ਣ": "nn"
+    },
+    {
+        "ਤ": "t",
+        "ਥ": "th",
+        "ਦ": "d",
+        "ਧ": "dh",
+        "ਨ": "n"
+    },
+    {
+        "ਪ": "p",
+        "ਫ": "ph",
+        "ਬ": "b",
+        "ਭ": "bh",
+        "ਮ": "m"
+    },
+    {
+        "ਯ": "y",
+        "ਰ": "r",
+        "ਲ": "l",
+        "ਵ": "v",
+        "ੜ": "rr"
+    }
+]
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Your code here
+    console.log('Document is ready!');
+    let alphabetData = [];
+
+    let container = document.getElementById('container');
+
+        renderGurmukhiAll(akhar);
+
+
+
+
+})();
